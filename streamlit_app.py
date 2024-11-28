b8afc03d014d1cea710fcfc26cf3dcb9ce1eca26
adarsh10082000 <165761316+adarsh10082000@users.noreply.github.com>
2024-11-28 17:39:24 +0530

    Update streamlit_app.py
    err 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+
+def diabetes_prediction(input_data):
+    
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
+    else:
+      return 'The person is diabetic'
+
+
 def main():
     
     
